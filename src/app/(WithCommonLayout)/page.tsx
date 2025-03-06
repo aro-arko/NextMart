1194098a75fdca6371b26744ed1bf13724e7a99c
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-03-07 02:46:47 +0800

    form fields

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to NextMart Home Page</h1>
+      <Button>Click Me</Button>
     </div>
   );
 };
--- src/components/modules/auth/register/RegisterForm.tsx
+++ src/components/modules/auth/register/RegisterForm.tsx
@@ -10,36 +10,107 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Link from "next/link";
+import Logo from "@/app/assets/svgs/Logo";
 
-const RegisterForm = () => {
+export default function RegisterForm() {
   const form = useForm();
 
-  const onSubmit = (data) => {
+  const {
+    formState: { isSubmitting },
+  } = form;
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
+
   return (
-    <div className="max-w-md">
+    <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-8 shadow-lg">
+      <div className="flex items-center space-x-4 mb-6">
+        <Logo />
+        <div>
+          <h1 className="text-2xl font-semibold text-black">Register</h1>
+          <p className="font-light text-sm text-black">
+            Join us today and start your journey!
+          </p>
+        </div>
+      </div>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
+        <form className="" onSubmit={form.handleSubmit(onSubmit)}>
           <FormField
             control={form.control}
             name="name"
             render={({ field }) => (
-              <FormItem>
-                <FormLabel />
+              <FormItem className="mb-4">
+                <FormLabel className="text-black">Name</FormLabel>
                 <FormControl>
                   <Input {...field} value={field.value || ""} />
                 </FormControl>
-                <FormMessage />
+                <FormMessage className="text-black" />
               </FormItem>
             )}
           />
-          <Button type="submit">Register</Button>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="mb-4">
+                <FormLabel className="text-black">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    {...field}
+                    value={field.value || ""}
+                    className="text-black"
+                  />
+                </FormControl>
+                <FormMessage className="text-black" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="mb-4">
+                <FormLabel className="text-black">Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage className="text-black" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => (
+              <FormItem className="mb-4">
+                <FormLabel className="text-black">Confirm Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} value={field.value || ""} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="mt-5 w-full bg-primary text-white py-2 rounded-lg hover:bg-primary-dark transition-colors"
+          >
+            {isSubmitting ? "Registering..." : "Register"}
+          </Button>
         </form>
       </Form>
+      <p className="text-sm text-black text-center my-4">
+        Already have an account?{" "}
+        <Link
+          href="/login"
+          className="text-primary font-medium hover:underline"
+        >
+          Login
+        </Link>
+      </p>
     </div>
   );
-};
-
-export default RegisterForm;
+}
