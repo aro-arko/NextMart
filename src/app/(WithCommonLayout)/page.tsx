f0c9ae61045215cf174bfe8d49d4729ae530c7df
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-03-08 14:42:08 +0800

    user context applied

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@/components/ui/button";
-import { getCurrentUser } from "@/services/AuthService";
+"use client";
+import { useUser } from "@/context/UserContext";
 
-const HomePage = async () => {
-  const user = await getCurrentUser();
+const HomePage = () => {
+  const user = useUser();
   console.log(user);
   return (
     <div>
       <h1>Welcome to NextMart Home Page</h1>
-      <Button>Click Me</Button>
     </div>
   );
 };
