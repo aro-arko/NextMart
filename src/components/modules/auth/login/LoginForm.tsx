df45eaa144b7fef949ff1480ee8f9a4dd5533ed8
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-03-08 14:02:43 +0800

    login form completed

---
+++ src/components/modules/auth/login/LoginForm.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Link from "next/link";
+import Logo from "@/app/assets/svgs/Logo";
+import { loginUser } from "@/services/AuthService";
+import { toast } from "sonner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "./loginValidation";
+
+export default function LoginForm() {
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const {
+    formState: { isSubmitting },
+  } = form;
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    try {
+      const res = await loginUser(data);
+      if (res.success) {
+        toast.success(res?.message);
+      } else {
+        toast.error(res?.message);
+      }
+      console.log(res);
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-8 shadow-lg bg-white">
+      <div className="flex items-center space-x-4 mb-6">
+        <Logo />
+        <div>
+          <h1 className="text-2xl font-semibold text-black">Login</h1>
+          <p className="font-light text-sm text-black">
+            Welcome back! Login to your account
+          </p>
+        </div>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="mb-4">
+                <FormLabel className="text-black">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    {...field}
+                    value={field.value || ""}
+                    className="text-black"
+                  />
+                </FormControl>
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="mb-4">
+                <FormLabel className="text-black">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    {...field}
+                    value={field.value || ""}
+                    className="text-black"
+                  />
+                </FormControl>
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="mt-5 w-full bg-primary text-white py-2 rounded-lg hover:bg-primary-dark transition-colors"
+          >
+            {isSubmitting ? "Logging..." : "Login"}
+          </Button>
+        </form>
+      </Form>
+      <p className="text-sm text-black text-center my-4">
+        Do not have an account?{" "}
+        <Link
+          href="/register"
+          className="text-primary font-medium hover:underline"
+        >
+          Register
+        </Link>
+      </p>
+    </div>
+  );
+}
